6c8410b592fb193382e0fc0af2586065430e66d4
bigrocs <bigrocs@qq.com>
2018-04-27 22:23:01 +0800

    测试

--- src/components/index/index.vue
+++ src/components/index/index.vue
@@ -1,7 +1,33 @@
 <template>
-    <div>
-        123
-    </div>
+  <Row :gutter="16">
+          <Col span="6">
+              <div>col-6</div>
+          </Col>
+          <Col span="6">
+              <div>col-6</div>
+          </Col>
+          <Col span="6">
+              <div>col-6</div>
+          </Col>
+          <Col span="6">
+              <div>col-6</div>
+          </Col>
+          <Col span="6">
+              <div>col-6</div>
+          </Col>
+          <Col span="6">
+              <div>col-6</div>
+          </Col>
+          <Col span="6">
+              <div>col-6</div>
+          </Col>
+          <Col span="6">
+              <div>col-6</div>
+          </Col>
+          <Col span="6">
+              <div>col-6</div>
+          </Col>
+  </Row>
 </template>
 
 <script>
@@ -9,6 +35,11 @@
   import gql from 'graphql-tag'
   export default {
     name: 'jvi-index',
+    data() {
+      return {
+        data: {},
+      }
+    },
     computed: {
       routeNmae() {
         return this.$route.name
@@ -20,27 +51,46 @@
     methods: {
       initJson(){
         let packageModel = this.routeNmae.split(":")
-        Cache.remember(this.routeNmae, async () => {
-          let apollo = await this.$apollo.query({
-            query: gql`query ($package: String!, $model: String!) {
-              model(package: $package, model: $model){
-                layout
-                data
-              }
-            }`,
-            variables: {
-              package: packageModel[0],
-              model: packageModel[1]
+        this.$apollo.query({
+          query: gql`query ($package: String!, $model: String!) {
+            model(package: $package, model: $model){
+              data
             }
-          })
-          return apollo.data.sidebar
-        } , 60*24*7).then((sidebar) => {
-          this.sidebar = sidebar
+          }`,
+          variables: {
+            package: packageModel[0],
+            model: packageModel[1]
+          }
+        }).then((data) => {
+          this.data = data.data.model[0].data
         })
+        // Cache.remember(this.routeNmae, async () => {
+        //   let apollo = await this.$apollo.query({
+        //     query: gql`query ($package: String!, $model: String!) {
+        //       model(package: $package, model: $model){
+        //         layout
+        //         data
+        //       }
+        //     }`,
+        //     variables: {
+        //       package: packageModel[0],
+        //       model: packageModel[1]
+        //     }
+        //   })
+        //   return apollo.data.model[0]
+        // } , 60*24*7).then((model) => {
+        //   console.log(model);
+        //   // this.layout = model[0].layout
+        //   // console.log(this.layout);
+        // })
       }
     }
   }
 </script>
 <style lang="scss" scoped>
-
+.ivu-col{
+  div{
+    background:rgba(0,153,229,.9);
+  }
+}
 </style>
